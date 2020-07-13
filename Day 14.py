0e3b82469d35b003e3d830dd0738cdb966ad09d1
Gunjan Giri <gunjangiri8410@gmail.com>
2020-07-13 20:14:16 +0530

    Day 14 Added

---
+++ Day 14.py
@@ -0,0 +1,6 @@
+a = str(input("The original string is : "))
+b = str(input("The substring you want to replace : "))
+c = str(input("The string you want to use instead : "))
+
+d = a.replace(b , c)
+print("My Name is " ,d)
